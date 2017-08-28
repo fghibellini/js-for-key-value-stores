bd51c5ed6b42ccba3270cbbf663dc0dcc677388a
Filippo Ghibellini <f.ghibellini@gmail.com>
2017-08-28 19:52:59 +0200

    Adds some structure to the project

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 
-export VEIGHT=/nix/store/mrv9b54w3b55swz9v2mdkzwk2hqkffsf-v8-5.4.232
-
-hello-world : ./hello-world.cc
-	bash ./build.sh
-	#third_party/icu/libicu{uc,i18n},src/inspector/libinspector}.a \
+bin/run : ./src/main.cpp
+	mkdir bin 2>/dev/null || true
+	./build.sh
--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+#!/usr/bin/env bash
 
 g++ -I"${v8}" -I"${v8}/include" \
-    hello-world.cc\
-    -o hello-world\
+    src/main.cpp\
+    -o bin/run\
     -Wl,--start-group \
 	${v8}/lib/libv8_{base,libbase,snapshot,libplatform,libsampler}.a\
     ${icu}/lib/libicu{uc,i18n}.so\
